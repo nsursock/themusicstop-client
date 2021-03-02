8ff7321d1be7fe32c20bd6fbad14276a8cfb074d
Nicolas Sursock (work) <nwsursock@Nicolass-MacBook-Pro.local>
2021-03-02 08:02:31 +0200

    added auth for members actions

--- src/components/music/Heading.vue
+++ src/components/music/Heading.vue
@@ -31,7 +31,7 @@
         </div>
       </div>
     </div>
-    <div class="mt-5 flex lg:mt-0 lg:ml-4">
+    <div class="mt-5 flex lg:mt-0 lg:ml-4" v-show="$store.getters.loggedIn">
 
       <span class="sm:ml-3">
         <button v-on:click="handleToggle" type="button" class="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
