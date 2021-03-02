4099c9f93adffc865caa1cc2018a2b798d3742e7
Nicolas Sursock (work) <nwsursock@Nicolass-MacBook-Pro.local>
2021-03-02 06:35:19 +0200

    added exit intent popup

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <Nav />
     <div @click="emitCloseMenu">
       <router-view></router-view>
+      <ExitIntentPopup @toggleExitPopup="toggleExitPopup" v-show="showPopup && !$store.getters.loggedIn"/>
       <FooterSection />
     </div>
   </div>
@@ -10,22 +11,49 @@
 <script>
 import Nav from './components/partials/Nav'
 import FooterSection from './components/FooterSection'
+import ExitIntentPopup from './components/ExitIntentPopup'
 import { bus } from '@/main'
 
 export default {
   name: 'app',
   components: {
     Nav,
-    FooterSection
+    FooterSection,
+    ExitIntentPopup
+  },
+  created() {
+    document.addEventListener("DOMContentLoaded", () => {
+      // desktop
+      document.addEventListener("mouseout", (event) => {
+        if (!event.toElement && !event.relatedTarget) {
+          setTimeout(() => {
+            this.showPopup = true;
+          }, 1000)
+        }
+      });
+      // mobile
+      window.addEventListener('scroll', (event) => {
+        if (!event.toElement && !event.relatedTarget) {
+          if (window.scrollY === 0) {
+            setTimeout(() => {
+              this.showPopup = true;
+            }, 1000)
+          }
+        }
+      }, {passive: true});
+    })
   },
   data () {
     return {
-
+      showPopup: false,
     }
   },
   methods: {
     emitCloseMenu: function() {
       bus.$emit('closeMenu');
+    },
+    toggleExitPopup() {
+      this.showPopup = !this.showPopup;
     }
   }
 }
