2d03cace4fd6596d586d5fe78f966ae6431f2ae4
Nicolas Sursock (work) <nwsursock@Nicolass-MacBook-Pro.local>
2021-03-06 18:27:12 +0200

    changed colors on splash

--- src/views/Splash.vue
+++ src/views/Splash.vue
@@ -82,7 +82,7 @@
 
         <div class="flex">
           <div class="flex-shrink-0">
-            <div class="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+            <div class="flex items-center justify-center h-12 w-12 rounded-md bg-purple-500 text-white">
               <!-- Heroicon name: scale -->
               <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 6l3 1m0 0l-3 9a5.002 5.002 0 006.001 0M6 7l3 9M6 7l6-2m6 2l3-1m-3 1l-3 9a5.002 5.002 0 006.001 0M18 7l3 9m-3-9l-6-2m0-2v2m0 16V5m0 16H9m3 0h3" />
@@ -120,7 +120,7 @@
 
         <div class="flex">
           <div class="flex-shrink-0">
-            <div class="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+            <div class="flex items-center justify-center h-12 w-12 rounded-md bg-purple-500 text-white">
               <!-- Heroicon name: annotation -->
               <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z" />
@@ -162,7 +162,7 @@
         </p>
       </div>
       <div class="rounded-md shadow mt-5">
-        <button @click="handleStart" class="w-full flex items-center justify-center  border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:text-lg py-2 px-3">
+        <button @click="handleStart" class="w-full flex items-center justify-center  border border-transparent text-base font-medium rounded-md text-white bg-purple-600 hover:bg-purple-700 md:text-lg py-2 px-3">
           Get started
         </button>
       </div>
@@ -250,7 +250,7 @@
   <div class="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
     <h2 class="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
       <span class="block">Want news and updates?</span>
-      <span class="block text-purple-600">Sign up for our weekly newsletter.</span>
+      <span class="block text-indigo-600">Sign up for our weekly newsletter.</span>
     </h2>
     <div class="mt-8 lex lg:mt-0 lg:flex-shrink-0 lg:w-1/2 mr-5 sm:mr-0">
       <form action="https://formspree.io/f/xjvplbll" method="POST" class="md:flex md:justify-end">
