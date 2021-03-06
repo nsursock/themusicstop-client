6f2225b7753588df176669f2017860b0e431236f
Nicolas Sursock (work) <nwsursock@Nicolass-MacBook-Pro.local>
2021-03-06 08:33:36 +0200

    created signup notifications

--- src/components/NotificationCard.vue
+++ src/components/NotificationCard.vue
@@ -1,8 +1,10 @@
 <template lang="html">
   <div class="fixed bottom-0 right-0 z-50 rounded-lg shadow-lg border border-gray-100 bg-gray-50 flex items-center justify-around p-4 m-3 w-11/12 md:w-1/4 ">
-    <svg v-if="!details.profileImage" class="h-12 w-12 rounded-full object-cover text-gray-300 bg-gray-600" fill="currentColor" viewBox="0 0 24 24">
-      <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
-    </svg>
+    <div v-if="!details.profileImage" class="">
+      <svg class="h-12 w-12 rounded-full object-cover text-gray-300 bg-gray-600" fill="currentColor" viewBox="0 0 24 24">
+        <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
+      </svg>
+    </div>
     <img v-else class="object-cover w-12 h-12 rounded-full" :src="details.profileImage" alt="">
     <div class="flex flex-col ml-3 items-start  justify-center">
       <span class="text-sm text-gray-800">
